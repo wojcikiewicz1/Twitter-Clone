5ea9e4ea417e6af4615f09e9c2cc09393041381b
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-03-08 15:44:33 +0100

    commit

--- src/main/java/com/example/Twitter/Clone/Follower/FollowerRepository.java
+++ src/main/java/com/example/Twitter/Clone/Follower/FollowerRepository.java
@@ -1,5 +1,6 @@
 package com.example.Twitter.Clone.Follower;
 
+import com.example.Twitter.Clone.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,4 +12,6 @@ public interface FollowerRepository extends JpaRepository <Follower, Long> {
 
     @Query("SELECT s FROM Follower s WHERE s.user.username = :username")
     List<Follower> findFollowingsByUsername (String username);
+
+    Follower findByUserAndUserToFollow(User user, User userToUnfollow);
 }
