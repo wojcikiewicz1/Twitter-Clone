d61f9559d774dea775d31d55a79cc45850aca34d
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-03-08 22:53:59 +0100

    frontend

---
+++ src/main/resources/static/javascript/follow.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.querySelectorAll('.btntofollow').forEach(button => {
+        button.addEventListener('click', function(e) {
+            const username = this.dataset.username;
+            const isFollowing = this.classList.contains('unfollowButton');
+            const url = `/api/${isFollowing ? 'unfollow' : 'follow'}`;
+            const headers = new Headers({
+                'Content-Type': 'application/x-www-form-urlencoded',
+            });
+
+            fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: `username=${username}`,
+                credentials: 'include'
+            })
+                .then(response => {
+                    if (response.ok) {
+                        if (isFollowing) {
+                            this.textContent = 'Follow';
+                            this.classList.remove('unfollowButton');
+                            this.classList.add('followButton');
+                            updateButtonStyles(this);
+                        } else {
+                            this.textContent = 'Unfollow';
+                            this.classList.add('unfollowButton');
+                            this.classList.remove('followButton');
+                            updateButtonStyles(this);
+                        }
+                    } else {
+                        throw new Error('Action failed');
+                    }
+                })
+                .catch(error => {
+                    console.error('Error:', error);
+                    alert('Action failed');
+                });
+        });
+    });
+});
+
+function updateButtonStyles(button) {
+    button.style = '';
+
+    if (button.classList.contains('followButton')) {
+        button.onmouseover = function() {
+            this.style.cursor = 'pointer';
+            this.style.backgroundColor = '#333333';
+        };
+        button.onmouseout = function() {
+            this.style.backgroundColor = '';
+        };
+    } else if (button.classList.contains('unfollowButton')) {
+        button.onmouseover = function() {
+            this.style.cursor = 'pointer';
+            this.style.backgroundColor = '#ff8c73';
+            this.style.color = 'red';
+            this.style.border = '1px solid red';
+        };
+        button.onmouseout = function() {
+            this.style.backgroundColor = '';
+            this.style.color = '';
+            this.style.border = '';
+        };
+    }
+}
