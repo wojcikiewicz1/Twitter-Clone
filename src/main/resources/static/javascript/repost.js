e06ce7c6f4ee8d2606e38f52c07a9d1f77293d01
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-03-19 21:02:12 +0100

    frontend

--- src/main/resources/static/javascript/repost.js
+++ src/main/resources/static/javascript/repost.js
@@ -1,29 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.querySelectorAll('.share-post').forEach(button => {
+    document.querySelectorAll('.retweet').forEach(button => {
         button.addEventListener('click', function(e) {
             e.preventDefault();
             e.stopPropagation();
 
             const postId = this.dataset.id;
+            const isReposted = this.classList.contains('unRetweetPost');
+            const urlPost = `/api/${isReposted ? 'unrepost' : 'repost'}`;
             const headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded',});
 
-            fetch(`/api/repost`, {
+            fetch( urlPost, {
                 method: 'POST',
                 headers: headers,
                 body: `postId=${postId}`,
                 credentials: 'include'
             }).then(response => {
                 if (response.ok) {
-                    alert('Post reposted!');
+                    return response.json();
                 } else {
                     throw new Error('Action failed');
+                }
+            }).then(data => {
+                const repostsCountSpan = this.querySelector('.postIcons2Numbers');
+                repostsCountSpan.textContent = ` ${data.repostsCount}`;
+
+                if (data.isReposted) {
+                    this.classList.remove('retweetPost');
+                    this.classList.add('unRetweetPost');
+                } else {
+                    this.classList.add('retweetPost');
+                    this.classList.remove('unRetweetPost');
                 }
             }).catch(error => {
                 console.error('Error:', error);
                 alert('Action failed');
             });
 
-    });
+        });
     });
 });
 
