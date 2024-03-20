443fb9a66fd9acd8ddc33188e371c6ce3b13d5f9
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-03-20 15:44:41 +0100

    frontend

--- src/main/resources/static/javascript/modals.js
+++ src/main/resources/static/javascript/modals.js
@@ -37,31 +37,137 @@ document.addEventListener('click', function(event) {
         });
     }
 });
-
-//--------------------------------Modal for adding posts------------------------
-if (document.body.id === 'homePage') {
-    homePostModal();
-} else if (document.body.id === 'profilePage') {
-    profilePostModal();
-} else if (document.body.id === 'postPage') {
-    postPostModal();
-} else if (document.body.id === 'commentPage') {
-    commentPostModal();
-}
-
-function homePostModal() {
-    let modal = document.getElementById("postModal");
-    let span = document.getElementsByClassName("close")[0];
+//------------------------------------------Modals functions------------------------------------------------------
+let modals = {};
+function initializeModalHandlers(modalId, closeButtonClass) {
+    let modal = document.getElementById(modalId);
+    let span = document.getElementsByClassName(closeButtonClass)[0];
 
     span.onclick = function () {
         modal.style.display = "none";
     }
 
-    window.onclick = function (event) {
-        if (event.target == modal) {
+    modals[modalId] = modal;
+}
+
+window.onclick = function (event) {
+    Object.keys(modals).forEach(function(key) {
+        let modal = modals[key];
+        if (event.target === modal) {
             modal.style.display = "none";
         }
-    }
+    });
+}
+
+if (document.body.id === 'homePage') {
+    homeCommentModal();
+    homePostModal();
+} else if (document.body.id === 'profilePage') {
+    profileCommentModal();
+    profilePostModal();
+} else if (document.body.id === 'postPage' || document.body.id === 'commentPage') {
+    postOrCommentCommentModal();
+    postOrCommentPostModal();
+}
+//--------------------------------Modal for adding comment----------------------------------------------------------
+document.querySelectorAll('.openCommentModal').forEach(item => {
+    item.addEventListener('click', event => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const postId = item.getAttribute('data-post-id');
+        const commentId = item.getAttribute('data-comment-id');
+        let myUser = {username: 'defaultUsername'};
+
+        const formAction = commentId ? `/${myUser.username}/comment/${commentId}` : `/${myUser.username}/${postId}`;
+        document.getElementById('commentForm').setAttribute('action', formAction);
+        document.getElementById('commentModal').style.display = 'block';
+    });
+});
+
+function homeCommentModal() {
+
+    initializeModalHandlers("commentModal", "closeCommentModal");
+
+    document.querySelectorAll('.openCommentModal').forEach(function(item) {
+        item.addEventListener('click', function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+            document.getElementById('commentModal').style.display = 'block';
+            document.querySelector('.topbar').style.position = 'relative';
+            document.querySelector('.topbar').style.zIndex = '-1';
+            document.querySelector('.search').style.position = 'relative';
+            document.querySelector('.search').style.zIndex = '-1';
+        });
+    });
+
+    document.querySelector('.closeCommentModal').onclick = function () {
+        document.getElementById('commentModal').style.display = 'none';
+        document.querySelector('.topbar').style.position = 'sticky';
+        document.querySelector('.topbar').style.zIndex = '1';
+        document.querySelector('.search').style.position = 'sticky';
+        document.querySelector('.search').style.zIndex = '1';
+    };
+}
+
+function profileCommentModal() {
+
+    initializeModalHandlers("commentModal", "closeCommentModal");
+
+    document.querySelectorAll('.openCommentModal').forEach(function(item) {
+        item.addEventListener('click', function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+        document.getElementById('commentModal').style.display = 'block';
+        document.querySelector('.top').style.position = 'relative';
+        document.querySelector('.top').style.zIndex = '-1';
+        document.querySelector('.profilemain').style.position = 'relative';
+        document.querySelector('.profilemain').style.zIndex = '-1';
+        document.querySelector('.search').style.position = 'relative';
+        document.querySelector('.search').style.zIndex = '-1';
+        });
+    });
+
+    document.querySelector('.closeCommentModal').onclick = function () {
+        document.getElementById('commentModal').style.display = 'none';
+        document.querySelector('.top').style.position = 'sticky';
+        document.querySelector('.top').style.zIndex = '1';
+        document.querySelector('.profilemain').style.position = 'sticky';
+        document.querySelector('.profilemain').style.zIndex = '1';
+        document.querySelector('.search').style.position = 'sticky';
+        document.querySelector('.search').style.zIndex = '1';
+    };
+}
+
+function postOrCommentCommentModal() {
+
+    initializeModalHandlers("commentModal", "closeCommentModal");
+
+    document.querySelectorAll('.openCommentModal').forEach(function(item) {
+        item.addEventListener('click', function (event) {
+            event.stopPropagation();
+            event.preventDefault();
+            document.getElementById('commentModal').style.display = 'block';
+            document.querySelector('.postTop').style.position = 'relative';
+            document.querySelector('.postTop').style.zIndex = '-1';
+            document.querySelector('.search').style.position = 'relative';
+            document.querySelector('.search').style.zIndex = '-1';
+        });
+    });
+
+    document.querySelector('.closeCommentModal').onclick = function () {
+        document.getElementById('commentModal').style.display = 'none';
+        document.querySelector('.postTop').style.position = 'sticky';
+        document.querySelector('.postTop').style.zIndex = '1';
+        document.querySelector('.search').style.position = 'sticky';
+        document.querySelector('.search').style.zIndex = '1';
+    };
+}
+
+//--------------------------------Modal for adding post---------------------------
+function homePostModal() {
+
+    initializeModalHandlers("postModal", "closePostModal");
 
     document.getElementById('openPostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'block';
@@ -71,7 +177,7 @@ function homePostModal() {
         document.querySelector('.search').style.zIndex = '-1';
     };
 
-    document.querySelector('.close').onclick = function () {
+    document.querySelector('.closePostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'none';
         document.querySelector('.topbar').style.position = 'sticky';
         document.querySelector('.topbar').style.zIndex = '1';
@@ -81,18 +187,8 @@ function homePostModal() {
 }
 
 function profilePostModal() {
-    let modal = document.getElementById("postModal");
-    let span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    initializeModalHandlers("postModal", "closePostModal");
 
     document.getElementById('openPostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'block';
@@ -104,7 +200,7 @@ function profilePostModal() {
         document.querySelector('.search').style.zIndex = '-1';
     };
 
-    document.querySelector('.close').onclick = function () {
+    document.querySelector('.closePostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'none';
         document.querySelector('.top').style.position = 'sticky';
         document.querySelector('.top').style.zIndex = '1';
@@ -115,19 +211,9 @@ function profilePostModal() {
     };
 }
 
-function postPostModal() {
-    let modal = document.getElementById("postModal");
-    let span = document.getElementsByClassName("close")[0];
+function postOrCommentPostModal() {
 
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+    initializeModalHandlers("postModal", "closePostModal");
 
     document.getElementById('openPostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'block';
@@ -137,7 +223,7 @@ function postPostModal() {
         document.querySelector('.search').style.zIndex = '-1';
     };
 
-    document.querySelector('.close').onclick = function () {
+    document.querySelector('.closePostModal').onclick = function () {
         document.getElementById('postModal').style.display = 'none';
         document.querySelector('.postTop').style.position = 'sticky';
         document.querySelector('.postTop').style.zIndex = '1';
@@ -146,36 +232,7 @@ function postPostModal() {
     };
 }
 
-function commentPostModal() {
-    let modal = document.getElementById("postModal");
-    let span = document.getElementsByClassName("close")[0];
 
-    span.onclick = function () {
-        modal.style.display = "none";
-    }
-
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
-
-    document.getElementById('openPostModal').onclick = function () {
-        document.getElementById('postModal').style.display = 'block';
-        document.querySelector('.postTop').style.position = 'relative';
-        document.querySelector('.postTop').style.zIndex = '-1';
-        document.querySelector('.search').style.position = 'relative';
-        document.querySelector('.search').style.zIndex = '-1';
-    };
-
-    document.querySelector('.close').onclick = function () {
-        document.getElementById('postModal').style.display = 'none';
-        document.querySelector('.postTop').style.position = 'sticky';
-        document.querySelector('.postTop').style.zIndex = '1';
-        document.querySelector('.search').style.position = 'sticky';
-        document.querySelector('.search').style.zIndex = '1';
-    };
-}
 //--------------------------------Textareas resizing------------------------
 document.addEventListener('DOMContentLoaded', function() {
     const textareas = document.querySelectorAll('.whatishappening, .postyourreply');
