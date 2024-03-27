ccd5b06112a87b981f0b1b3918dbbffaee37bf33
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-03-27 12:44:08 +0100

    frontend

--- src/main/resources/static/javascript/modals.js
+++ src/main/resources/static/javascript/modals.js
@@ -285,19 +285,32 @@ function profileEditModal() {
         document.querySelector('.search').style.zIndex = '1';
     };
 }
-//------------------------------------------------Textareas resizing----------------------------------------------------
+//--------------------------------------Textareas & inputs resizing and lengths------------------------------------------
 document.addEventListener('DOMContentLoaded', function() {
-    const textareas = document.querySelectorAll('.whatishappening, .postyourreply');
+    const textareas = document.querySelectorAll('.whatishappening, .postyourreply, .form-control');
 
     textareas.forEach(textarea => {
         textarea.addEventListener('input', autoResize, false);
     });
+
     function autoResize() {
         console.log("Auto resizing...");
         this.style.height = 'auto';
         this.style.height = this.scrollHeight + 'px';
     }
+
+    const inputs = document.querySelectorAll('input.form-control');
+
+    inputs.forEach(input => {
+        input.addEventListener('input', autoResize, false)
+    });
 });
+
+function limitInputLength(element, maxLength) {
+    if (element.value.length > maxLength) {
+        element.value = element.value.slice(0, maxLength);
+    }
+}
 
 
 //-----------------------------------Refreshing page after adding post in modal-----------------------------------------
