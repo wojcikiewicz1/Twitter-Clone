e87399fa1e21a56fad0eb4e7a17f0b34e11dedb7
Bartosz Wójcikiewicz <bartoszwojcikiewicz1@gmail.com>
2024-04-04 13:05:39 +0200

    frontend

--- src/main/resources/static/javascript/others.js
+++ src/main/resources/static/javascript/others.js
@@ -1,7 +1,11 @@
-//---------------------------------------------Active tab color---------------------------------------------------------
-document.addEventListener("DOMContentLoaded", function() {
-    const path = window.location.pathname;
+document.addEventListener('DOMContentLoaded', function() {
+    setActiveTabColor();
+    autoResizeSetup();
+    pinUnpinSetup();
+});
 
+function setActiveTabColor() {
+    const path = window.location.pathname;
     const followersTab = document.querySelector("#for-you");
     const followingTab = document.querySelector("#following");
 
@@ -10,13 +14,18 @@ document.addEventListener("DOMContentLoaded", function() {
     } else if (path.includes("/following")) {
         followingTab.classList.add("activeTab");
     }
-});
-//--------------------------------------Textareas & inputs resizing and lengths------------------------------------------
-document.addEventListener('DOMContentLoaded', function() {
+}
+
+function autoResizeSetup() {
     const textareas = document.querySelectorAll('.whatishappening, .postyourreply, .form-control');
 
     textareas.forEach(textarea => {
         textarea.addEventListener('input', autoResize, false);
+    });
+
+    const inputs = document.querySelectorAll('input.form-control');
+    inputs.forEach(input => {
+        input.addEventListener('input', autoResize, false);
     });
 
     function autoResize() {
@@ -24,28 +33,24 @@ document.addEventListener('DOMContentLoaded', function() {
         this.style.height = 'auto';
         this.style.height = this.scrollHeight + 'px';
     }
-
-    const inputs = document.querySelectorAll('input.form-control');
-
-    inputs.forEach(input => {
-        input.addEventListener('input', autoResize, false)
-    });
-});
-
-function limitInputLength(element, maxLength) {
-    if (element.value.length > maxLength) {
-        element.value = element.value.slice(0, maxLength);
-    }
 }
 
-//--------------------------------------------Pin/Unpin post or comment------------------------------------------------
-document.addEventListener('DOMContentLoaded', function () {
-    const updatePinStatus = (element, isPinned) => {
-        element.dataset.isPinned = isPinned ? 'false' : 'true';
-        element.innerHTML = !isPinned ? 'Unpin' : 'Pin';
-    };
+function pinUnpinSetup() {
+    document.querySelectorAll('.post-pinoption, .comment-pinoption').forEach(button => {
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+            const isComment = this.classList.contains('comment-pinoption');
+            const id = this.dataset.id;
+            const isPinned = this.dataset.isPinned === 'true';
+            const actionType = isPinned ? 'unpin' : 'pin';
+            const entityType = isComment ? 'Comment' : 'Post';
+            const actionUrl = `/api/${actionType}${entityType}/${id}`;
 
-    const handlePinClick = (actionUrl, element) => {
+            handlePinClick(actionUrl, this);
+        });
+    });
+
+    function handlePinClick(actionUrl, element) {
         fetch(actionUrl, {
             method: 'POST',
             credentials: 'include'
@@ -61,20 +66,16 @@ document.addEventListener('DOMContentLoaded', function () {
             console.error('Error:', error);
             alert('Action failed');
         });
-    };
+    }
 
-    document.querySelectorAll('.post-pinoption, .comment-pinoption').forEach(button => {
-        button.addEventListener('click', function(e) {
-            e.preventDefault();
+    function updatePinStatus(element, isPinned) {
+        element.dataset.isPinned = isPinned ? 'false' : 'true';
+        element.innerHTML = !isPinned ? 'Unpin' : 'Pin';
+    }
+}
 
-            const isComment = this.classList.contains('comment-pinoption');
-            const id = this.dataset.id;
-            const isPinned = this.dataset.isPinned === 'true';
-            const actionType = isPinned ? 'unpin' : 'pin';
-            const entityType = isComment ? 'Comment' : 'Post';
-            const actionUrl = `/api/${actionType}${entityType}/${id}`;
-
-            handlePinClick(actionUrl, this);
-        });
-    });
-});
+function limitInputLength(element, maxLength) {
+    if (element.value.length > maxLength) {
+        element.value = element.value.slice(0, maxLength);
+    }
+}
